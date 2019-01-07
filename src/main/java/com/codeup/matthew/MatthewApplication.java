c541561e55e303305415186ac3f87689c66c4590
MattBrozusky <matthew.brozusky@gmail.com>
2019-01-07 11:47:13 -0600

    first commit

---
+++ src/main/java/com/codeup/matthew/MatthewApplication.java
@@ -0,0 +1,14 @@
+package com.codeup.matthew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatthewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatthewApplication.class, args);
+    }
+
+}
+
