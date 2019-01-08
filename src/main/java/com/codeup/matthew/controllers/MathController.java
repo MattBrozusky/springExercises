52bc4c5d7c292024af3b352f3a0a41942e5fff8d
MattBrozusky <matthew.brozusky@gmail.com>
2019-01-08 12:14:37 -0600

    finished views exercise

--- src/main/java/com/codeup/matthew/controllers/MathController.java
+++ src/main/java/com/codeup/matthew/controllers/MathController.java
@@ -1,30 +1,51 @@
 package com.codeup.matthew.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.Random;
+
+@Controller
 public class MathController {
 
-    @GetMapping("/add/{num1}/and/{num2}")
-    public int add(@PathVariable int num1, @PathVariable int num2){
-        return num1 + num2;
+//    @GetMapping("/add/{num1}/and/{num2}")
+//    public int add(@PathVariable int num1, @PathVariable int num2){
+//        return num1 + num2;
+//    }
+//
+//    @GetMapping("/subtract/{num1}/from/{num2}")
+//    public int subtract(@PathVariable int num1, @PathVariable int num2){
+//        return num1 - num2;
+//    }
+//
+//    @GetMapping("/multiply/{num1}/and/{num2}")
+//    public int multiply(@PathVariable int num1, @PathVariable int num2){
+//        return num1 * num2;
+//    }
+//
+//    @GetMapping("/divide/{num1}/by/{num2}")
+//    public int divide(@PathVariable int num1, @PathVariable int num2){
+//        return num1 / num2;
+//    }
+
+    @GetMapping("/roll-dice")
+    public String rollDice(){
+        return "roll-dice";
     }
 
-    @GetMapping("/subtract/{num1}/from/{num2}")
-    public int subtract(@PathVariable int num1, @PathVariable int num2){
-        return num1 - num2;
-    }
+    @GetMapping("/roll-dice/{n}")
+    public String rolledDicePage(@PathVariable int n, Model model){
+        int randomNum = (int)(Math.random() * 6 + 1);
+        model.addAttribute("randomNum", randomNum);
+        model.addAttribute("guess", n);
 
-    @GetMapping("/multiply/{num1}/and/{num2}")
-    public int multiply(@PathVariable int num1, @PathVariable int num2){
-        return num1 * num2;
-    }
-
-    @GetMapping("/divide/{num1}/by/{num2}")
-    public int divide(@PathVariable int num1, @PathVariable int num2){
-        return num1 / num2;
+        if (randomNum == n){
+            model.addAttribute("equal", true);
+        } else {
+            model.addAttribute("equal", false);
+        }
+        return "roll-dice-n";
     }
 
 }
--- src/main/java/com/codeup/matthew/controllers/PostController.java
+++ src/main/java/com/codeup/matthew/controllers/PostController.java
@@ -1,21 +1,39 @@
 package com.codeup.matthew.controllers;
 
+import com.codeup.matthew.beans.Post;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.ArrayList;
+
+@Controller
 public class PostController {
 
     @GetMapping("/posts")
-    public String index(){
-        return "Here are all the Posts";
+    public String index(Model model){
+
+
+        Post test = new Post("My first post", "This is super informative post",1);
+        Post test2 = new Post("My second post", "This is not an informative post", 2);
+        Post test3 = new Post("My third post", "This is a terrible post", 3);
+        ArrayList<Post> postsList = new ArrayList<Post>();
+        postsList.add(test);
+        postsList.add(test2);
+        postsList.add(test3);
+
+        model.addAttribute("posts", postsList);
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    public String singlePost(@PathVariable long id){
-        return "Here is the post for " + id;
+    public String singlePost(@PathVariable long id, Model model){
+        Post test = new Post("My first post", "This is super informative post",1);
+        model.addAttribute("post", test);
+
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
